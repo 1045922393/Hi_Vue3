394b4be9a53efb46b4cf9fb363a05e3434e47592
v_weiyehu <v_weiyehu@tencent.com>
2022-06-17 18:12:28 +0800

    feat: shining

---
+++ src/components/myButton.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="button">
+    <div class="text">{{ props.text }}</div>
+  </div>
+</template>
+<script lang="ts" setup>
+//@@description@@
+// ==================================== 一、组件类 ====================================
+// ==================================== 二、变量类 ====================================
+// ==================================== 三、方法类 ====================================
+import { defineProps } from "vue";
+// ==================================== 四、API类  ====================================
+const props = defineProps({
+  text: {
+    type: String,
+    default: "Loading",
+  },
+});
+</script>
+<style scoped lang="less">
+.button {
+  margin: 0 auto;
+  font-size: 40px;
+  line-height: 80px;
+  height: 80px;
+  text-align: center;
+  width: 300px;
+  position: relative;
+  background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+  border-radius: 30px;
+  background-size: 400%;
+  color: #fff;
+  z-index: 2;
+  font-weight: 800;
+  animation: colorChange 10s linear infinite;
+  // background-clip: text;
+  // -webkit-background-clip: text;
+  // -webkit-text-fill-color: transparent;
+}
+
+.button:before {
+  content: "";
+  position: absolute;
+  top: -5px;
+  left: -5px;
+  right: -5px;
+  bottom: -5px;
+  z-index: 1;
+  background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+  background-size: 400%;
+  border-radius: 40px;
+  filter: blur(40px);
+}
+
+.text {
+  font-size: 40px;
+  line-height: 80px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: linear-gradient(0deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+  border-radius: 30px;
+  background-size: 400%;
+  color: #fff;
+  z-index: 5;
+  font-weight: 800;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: colorChange 10s linear infinite;
+}
+
+@keyframes colorChange {
+  0% {
+    background-position: 0%;
+  }
+  100% {
+    background-position: 400%;
+  }
+}
+</style>
--- src/view/shining.vue
+++ src/view/shining.vue
@@ -1,14 +1,251 @@
 <template>
-  <div class="shinning">闪灵快手</div>
+  <div class="shinning">
+    <div class="types">
+      <template v-for="imgItem in components">
+        <component :is="imgItem" @click="showDialog(imgItem)"></component>
+      </template>
+    </div>
+    <MyButton
+      class="my_button"
+      text="START"
+      @click="gameDialog = true"
+    ></MyButton>
+  </div>
+  <div class="dialog" v-if="gameDialog">
+    <span class="close" @click="gameDialog = false">x</span>
+    <div class="showCard" @click="init">
+      <transition name="fade" mode="default">
+        <div v-show="showImg" class="card-face">
+          <component class="firstImg" :is="listData[0]"></component>
+          <component class="secondImg" :is="listData[1]"></component>
+        </div>
+      </transition>
+    </div>
+  </div>
+  <div class="dialog" v-if="dialogShow">
+    <span class="close" @click="dialogShow = false">x</span>
+    <component :is="screenImg"></component>
+  </div>
 </template>
 <script lang="ts" setup>
+import MyButton from "../components/myButton.vue";
+import book from "./shining/book.vue";
+import bottle from "./shining/bottle.vue";
+import ghost from "./shining/ghost.vue";
+import mouse from "./shining/mouse.vue";
+import sofa from "./shining/sofa.vue";
 //@@description@@
 // ==================================== 一、组件类 ====================================
 // ==================================== 二、变量类 ====================================
 // ==================================== 三、方法类 ====================================
+import { onMounted, ref, Ref, reactive, computed, watch } from "vue";
 // ==================================== 四、API类  ====================================
+const listData: Ref<Array<any>> = ref([]);
+const showImg = ref(false);
+const dialogShow = ref(false);
+const gameDialog = ref(false);
+const type = ["老鼠", "酒瓶", "幽灵", "沙发", "书本"],
+  components = [mouse, bottle, ghost, sofa, book],
+  color = ["#B2ABAC", "#279152", "#F8EDD2", "#ff001e", "#3883e1"],
+  showColor = ref(["#B2ABAC", "#279152", "#F8EDD2", "#ff001e", "#3883e1"]);
+const imgTop = ref("0");
+const img2Top = ref("0");
+const imgDeg = ref("0");
+const img2Deg = ref("0");
+const mouseColor = computed(() => showColor.value[0]);
+const bottleColor = computed(() => showColor.value[1]);
+const ghostColor = computed(() => showColor.value[2]);
+const sofaColor = computed(() => showColor.value[3]);
+const bookColor = computed(() => showColor.value[4]);
+// 获得随机数(可排除)
+const randomExcept = (indexArr: Array<number> = []) => {
+  const tempIndex = (Math.random() * 5) | 0;
+  if (indexArr.includes(tempIndex)) return randomExcept(indexArr);
+  return tempIndex;
+};
+
+const randomBoolean = () => {
+  return ((Math.random() * 2) | 0) === 1;
+};
+
+const screenImg = ref(null);
+const showDialog = (showImg) => {
+  dialogShow.value = true;
+  screenImg.value = showImg;
+};
+
+const getExist = () => {
+  //  情况一 存在
+  const firstIndex = randomExcept();
+  const secondeIndex = firstIndex;
+  const thirdIndex = randomExcept([firstIndex]);
+  const forthIndex = randomExcept([firstIndex, thirdIndex]);
+  showColor.value[firstIndex] = color[secondeIndex];
+  showColor.value[thirdIndex] = color[forthIndex];
+  const resultArr = [components[firstIndex], components[thirdIndex]];
+  return randomBoolean() ? resultArr : resultArr.reverse();
+};
+
+const nonentity = () => {
+  // 情况二 不存在
+  const firstIndex = randomExcept();
+  const secondeIndex = randomExcept([firstIndex]);
+  const thirdIndex = randomExcept([firstIndex, secondeIndex]);
+  const forthIndex = randomExcept([firstIndex, secondeIndex, thirdIndex]);
+  const resultArr = [components[firstIndex], components[thirdIndex]];
+  showColor.value[firstIndex] = color[secondeIndex];
+  showColor.value[thirdIndex] = color[forthIndex];
+  return randomBoolean() ? resultArr : resultArr.reverse();
+};
+
+const showCard = () => {
+  return randomBoolean() ? getExist() : nonentity();
+};
+
+let timeId: any = null;
+const init = () => {
+  showImg.value = false;
+  if (timeId) clearInterval(timeId);
+  const time = Math.floor(Math.random() * 4) + 2;
+  timeId = setTimeout(() => {
+    imgTop.value = Math.random() * 400 + "px";
+    img2Top.value = Math.random() * 400 + "px";
+    listData.value = showCard();
+    imgDeg.value = Math.floor(Math.random() * 360) + 1 + "deg";
+    img2Deg.value = Math.floor(Math.random() * 360) + 1 + "deg";
+    showImg.value = true;
+  }, time * 1000);
+};
+
+watch(
+  () => gameDialog.value,
+  (newValue) => {
+    if (newValue) init();
+  },
+);
+
+onMounted(() => {});
 </script>
 <style scoped lang="less">
+img,
+/deep/svg {
+  width: 200px !important;
+  height: 200px !important;
+}
 .shinning {
+  .types {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+  .my_button {
+    margin-top: 60px;
+    &:active {
+      box-shadow: 0px 0px 3px 3px #666 inset;
+      transform: translate(2px, 2px);
+    }
+  }
+}
+.showCard {
+  position: relative;
+  margin: 0 auto;
+  width: 100vw;
+  max-width: 600px;
+  height: 600px;
+  border-radius: 20px;
+  background-color: rgba(0, 0, 0, 0.75);
+  border: 10px solid yellow;
+  transition: 1s all;
+  transform-style: preserve-3d;
+
+  .card-face {
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+
+  &.flip {
+    transform: rotateY(180deg);
+  }
+  // 改变svg颜色
+  /deep/svg {
+    // 老鼠
+    path[fill="#B2ABAC"] {
+      fill: v-bind(mouseColor);
+    }
+    // 酒瓶
+    path[fill="#279152"] {
+      fill: v-bind(bottleColor);
+    }
+
+    // 幽灵
+    path[fill="#F8EDD2"] {
+      // fill: red;
+      fill: v-bind(ghostColor);
+    }
+
+    // 沙发
+    path[fill="#ff001e"] {
+      // fill: green;
+      fill: v-bind(sofaColor);
+    }
+
+    // book
+    path[fill="#3883e1"] {
+      // fill: red;
+      fill: v-bind(bookColor);
+    }
+  }
+
+  .firstImg,
+  .secondImg {
+    position: absolute;
+  }
+  .firstImg {
+    left: 50px;
+    top: v-bind(imgTop);
+    transform: rotate(v-bind(imgDeg));
+  }
+  .secondImg {
+    right: 50px;
+    top: v-bind(img2Top);
+    transform: rotate(v-bind(img2Deg));
+  }
+}
+
+.fade-enter,
+.fade-leave-to {
+  //进入前，离开后
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave {
+  //进入后，离开前
+  opacity: 1;
+}
+.fade-leave-active,
+.fade-enter-active {
+  transition: all 1s;
+}
+.dialog {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // width: 100vw;
+  // height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  z-index: 9999;
+
+  .close {
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    font-size: 30px;
+  }
 }
 </style>
