8ebe3288a33db4175241d478bcabe3977c3c6948
v_weiyehu <v_weiyehu@tencent.com>
2022-06-20 11:59:28 +0800

    feat：四子连棋 可撤销

--- src/view/fourChess.vue
+++ src/view/fourChess.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="chess_out">
-    <div class="legend">
-      <div class="legendItem" :class="{ highLight: turnPlay1 }">
-        <span class="play1 legendIcon"></span>玩家1
+  <div class="outBoard">
+    <div class="chess_out">
+      <div class="legend">
+        <div class="legendItem color1" :class="{ highLight: turnPlay1 }">
+          <span class="play1 legendIcon"></span>玩家1
+        </div>
+        <div class="legendItem color2" :class="{ highLight: !turnPlay1 }">
+          <span class="play2 legendIcon"></span>玩家2
+        </div>
       </div>
-      <div class="legendItem" :class="{ highLight: !turnPlay1 }">
-        <span class="play2 legendIcon"></span>玩家2
+      <div class="tools">
+        <div @click="goBack" :class="{ disabled: banLastStep }">上一步</div>
       </div>
-    </div>
-    <div
-      class="column"
-      v-for="(col, index) in chessBoard"
-      :key="index"
-      @click="playChess(index)"
-    >
       <div
-        class="chess"
-        :class="{ play1: chess === 1, play2: chess === 2 }"
-        v-for="(chess, chessIndex) in col"
-        :key="chessIndex"
-      ></div>
+        class="column"
+        v-for="(col, index) in chessBoard"
+        :key="index"
+        @click="playChess(index)"
+      >
+        <div
+          class="chess"
+          :class="{ play1: chess === 1, play2: chess === 2 }"
+          v-for="(chess, chessIndex) in col"
+          :key="chessIndex"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,7 +33,7 @@
 // ==================================== 一、组件类 ====================================
 // ==================================== 二、变量类 ====================================
 // ==================================== 三、方法类 ====================================
-import { ref, Ref, onMounted, computed } from "vue";
+import { ref, Ref, onMounted, computed, reactive } from "vue";
 // ==================================== 四、API类  ====================================
 
 const play1 = ref(1);
@@ -56,17 +61,41 @@ onMounted(() => {
   initBoard();
 });
 
+const lastStep = reactive({
+  lastCol: -1,
+  lastChess: -1,
+});
+
 const playChess = (col: number) => {
   const targetCol: Array<number> = chessBoard.value[col];
   const targetChessIndex = targetCol.findIndex((item) => item === 0);
   if (targetChessIndex === -1) return;
+  lastStep.lastCol = col;
+  lastStep.lastChess = targetChessIndex;
   targetCol[targetChessIndex] = playerPlaying.value;
+  turnPlay1.value = !turnPlay1.value;
+};
+const banLastStep = computed(() => {
+  if ([lastStep.lastCol, lastStep.lastChess].includes(-1)) return true;
+  return false;
+});
+const goBack = () => {
+  if (banLastStep.value) return;
+  chessBoard.value[lastStep.lastCol][lastStep.lastChess] = 0;
+  lastStep.lastCol = -1;
+  lastStep.lastChess = -1;
   turnPlay1.value = !turnPlay1.value;
 };
 </script>
 <style scoped lang="less">
+.outBoard {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  background-color: #333;
+}
 .chess_out {
-  --borderPadding: 50px;
+  --borderPadding: 2vw;
   position: relative;
   background-color: #333;
   border-radius: 8px;
@@ -102,17 +131,23 @@ const playChess = (col: number) => {
   }
 
   .legend {
-    position: absolute;
+    position: fixed;
     top: 10px;
     left: 10px;
-    color: #f7f9f7;
+    color: rgb(255, 2, 196);
     display: flex;
     .legendItem {
       display: flex;
       justify-content: start;
       align-items: center;
+      &.color1 {
+        color: #1500d9;
+      }
+      &.color2 {
+        color: #ff1744;
+      }
       &.highLight {
-        color: yellowgreen;
+        transform: scale(1.3);
       }
     }
 
@@ -120,7 +155,20 @@ const playChess = (col: number) => {
       display: inline-block;
       width: 14px;
       height: 14px;
-      margin: 0 10px;
+      margin: 0 4px 0 16px;
+      border-radius: 5px;
+    }
+  }
+
+  .tools {
+    position: fixed;
+    left: 2vw;
+    bottom: 2vh;
+    color: #fff;
+    font-size: 2rem;
+
+    .disabled {
+      opacity: 0;
     }
   }
 
