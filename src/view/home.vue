e55b17760e0e43e4450281110dc5552dfaba4c44
huweiye <1045922393@qq.com>
2022-08-19 18:12:32 +0800

    style: 首页

--- src/view/home.vue
+++ src/view/home.vue
@@ -4,11 +4,13 @@
       <div
         class="game_box hide_img"
         :class="{
+          current: index === centerIndex,
           leftImg: index === leftIndex,
           rightImg: index === rightIndex,
           centerImg: index === centerIndex,
           leftBackImg: index === leftBackIndex && routerComp.length > 4,
           rightBackImg: index === rightBackIndex && routerComp.length > 4,
+          blur: index !== centerIndex,
         }"
         v-for="(item, index) in routerComp"
         :key="item.name"
@@ -30,21 +32,22 @@
 //@@description@@ 主页 导航页
 // ==================================== 一、组件类 ====================================
 // ==================================== 二、变量类 ====================================
-import { routes } from "@/route/routes";
-import { useRouter } from "vue-router";
+import { routes } from '@/route/routes';
+import { useRouter } from 'vue-router';
 // ==================================== 三、方法类 ====================================
-import { rndColor } from "../utils";
+import { rndColor } from '../utils';
 // import stringRender from "json-templater/string";
 // import objectRender from "json-templater/object";
 // import jsonTemp from "../json/template.json";
-import { onMounted, computed, ref } from "vue";
+import { onBeforeUnmount, onMounted, computed, ref, watch } from 'vue';
 // ==================================== 四、API类  ====================================
 const routerComp = computed(() => routes.filter((item) => !item.hide));
 onMounted(() => {
-  const box: any = document.querySelector(".v_home");
+  const box: any = document.querySelector('.v_home');
   box.style.background = rndColor();
 });
 
+const hueValue = ref(0);
 const centerIndex = ref(0);
 const leftIndex = computed(() => {
   let temp = centerIndex.value - 1;
@@ -85,6 +88,26 @@ const clickRouter = (index, path) => {
   }
   centerIndex.value = index;
 };
+
+const huValueComp = computed(() => {
+  return hueValue.value + 'deg';
+});
+
+const symbolNum = ref(20);
+
+const unwatch = watch(hueValue, (newVal) => {
+  if (newVal > 360 || newVal < -360) symbolNum.value = symbolNum.value * -1;
+});
+
+let intervalId: any = setInterval(() => {
+  hueValue.value = hueValue.value + symbolNum.value;
+}, 2000);
+
+onBeforeUnmount(() => {
+  clearInterval(intervalId);
+  intervalId = null;
+  unwatch();
+});
 </script>
 <style scoped lang="less">
 .v_home {
@@ -110,54 +133,59 @@ const clickRouter = (index, path) => {
     // line-height: 100px;
     text-align: center;
     text-decoration: none;
-    box-shadow: 1px 1px 1px 1px #eeefff;
     display: flex;
     justify-content: center;
     align-items: center;
     width: 40vw;
     min-height: 40vw;
-    border-radius: 10px;
+    border-radius: 100px;
     font-size: 5vw;
     font-weight: 800;
     color: #fff;
     background-color: black;
-    border: 1px solid #fff;
+    border: none;
     perspective: 800px;
     transform-style: preserve-3d;
     transition: all 1.5s;
     left: 50%;
     top: 50%;
   }
+  .blur {
+    filter: blur(6px);
+  }
+
+  .current {
+    filter: hue-rotate(v-bind(huValueComp));
+    background: paleturquoise;
+    box-shadow: 0px 0px 10px 10px paleturquoise;
+  }
   .hide_img {
-    transform: perspective(800px) translateX(-110%) translateZ(-1000px)
-      translateY(-50%);
+    transform: perspective(800px) translateX(-110%) translateZ(-1000px) translateY(-50%) scale(0.7);
     z-index: 7;
   }
 
   .leftBackImg {
-    transform: perspective(800px) translateX(-215%) translateZ(-800px)
-      translateY(-50%) rotateY(-150deg);
+    transform: perspective(800px) translateX(-215%) translateZ(-800px) translateY(-60%)
+      rotateY(-150deg) scale(0.8);
     z-index: 8;
   }
   .rightBackImg {
-    transform: perspective(800px) translateX(15%) translateZ(-800px)
-      translateY(-50%) rotateY(150deg);
+    transform: perspective(800px) translateX(15%) translateZ(-800px) translateY(-60%)
+      rotateY(150deg) scale(0.8);
     z-index: 8;
   }
 
   .leftImg {
-    transform: perspective(800px) translateX(-200%) translateZ(-400px)
-      translateY(-50%) rotateY(-30deg);
+    transform: perspective(800px) translateX(-200%) translateZ(-400px) translateY(-60%)
+      rotateY(-30deg);
     z-index: 9;
   }
   .rightImg {
-    transform: perspective(800px) translateX(65%) translateZ(-400px)
-      translateY(-50%) rotateY(30deg);
+    transform: perspective(800px) translateX(65%) translateZ(-400px) translateY(-60%) rotateY(30deg);
     z-index: 9;
   }
   .centerImg {
-    transform: perspective(800px) translateX(-50%) translateZ(0)
-      translateY(-50%);
+    transform: perspective(800px) translateX(-50%) translateZ(0) translateY(-50%);
     z-index: 10;
   }
 }
